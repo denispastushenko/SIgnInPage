8d5388a341ef113ac4f90212fd90604d387c4e20
denispastushenko <dpastushenko1996@gmail.com>
2016-10-12 00:04:47 +0300

    234

--- src/main/java/utils/ProxyServerUtils.java
+++ src/main/java/utils/ProxyServerUtils.java
@@ -1,18 +1,38 @@
 package utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import example.Example;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponse;
-import org.littleshoot.proxy.*;
+import org.littleshoot.proxy.DefaultHttpProxyServer;
+import org.littleshoot.proxy.HttpFilter;
+import org.littleshoot.proxy.HttpProxyServer;
+import org.littleshoot.proxy.HttpResponseFilters;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class ProxyServerUtils {
     private static HttpProxyServer httpProxyServer;
 
-    public static void startServer() {
+    public static void startServer() throws IOException {
         HttpResponseFilters responseFilter = new HttpResponseFilters() {
             public HttpFilter getFilter(String s) {
                 return new HttpFilter() {
                     public HttpResponse filterResponse(HttpRequest httpRequest, HttpResponse httpResponse) {
-                        System.out.println(httpRequest.getUri());
+
+                        if (httpRequest.getUri().contains("PriceAvailability?SKU=0500285&")) {
+                            try {
+                                ObjectMapper objectMapper = new ObjectMapper();
+                                Example example =  objectMapper.readValue(httpResponse.getContent().array(), Example[].class)[0];
+                                example.setPrice(108.8);
+                                httpResponse.setContent(ChannelBuffers.copiedBuffer(objectMapper.writeValueAsString(example), Charset.defaultCharset()));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            System.out.println( new String(httpResponse.getContent().array()));
+                        }
                         return httpResponse;
                     }
 
@@ -21,12 +41,14 @@ public class ProxyServerUtils {
                     }
 
                     public boolean filterResponses(HttpRequest httpRequest) {
+
                         return true;
                     }
                 };
             }
         };
         httpProxyServer = new DefaultHttpProxyServer(1128, responseFilter);
+
         httpProxyServer.start();
     }
 }
